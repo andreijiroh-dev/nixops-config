98dcec0f757919f8b7e47bab9701ab68124b03a0
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-10-16 08:00:45 +0000

    cleanup and stuff
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

--- shared/home-manager/desktop.nix
+++ shared/home-manager/desktop.nix
@@ -5,9 +5,16 @@
   home.packages = with pkgs; [
     _1password-gui
     firefoxpwa
+    rpi-imager
     kdePackages.krfb
     #mysql-workbench
     remmina
     gparted
+
+    # workaround: https://discourse.nixos.org/t/need-help-with-resolving-missing-dependencies-for-auto-patchelf-on-termius/69722/2?u=ajhalili2006
+    (pkgs.termius.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs or [ ] ++ [ pkgs.sqlite ];
+    }))
+    zed-editor
   ];
 }
