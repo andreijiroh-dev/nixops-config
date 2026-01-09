d43c65ba97a6ccc32bbd4ecb0ff5d88de2761b1b
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2026-01-10 00:38:54 +0800

    chore(idx): setup devenv for Firebase Studio (fka Project IDX)
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

---
+++ .idx/dev.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, ... }: {
+  # Allow local config imports
+  improts = lib.optionals (builtins.pathExists ./dev.local.nix) [
+    ./dev.local.nix
+  ];
+
+  # Stick with unstable channel as usual
+  channel = "unstable";
+
+  packages = with pkgs; [
+    gitFull
+    github-cli
+    glab
+    shellcheck
+    shfmt
+    hadolint
+
+    nil
+    nixfmt
+  ];
+
+  idx = {
+    extensions = [
+      "jnoortheen.nix-ide"
+      "mads-hartmann.bash-ide-vscode"
+      "bbenoist.nix"
+      "tamasfe.even-better-toml"
+      "timonwong.shellcheck"
+    ];
+    workspace = {
+      onCreate.default.openFiles = [ "flake.nix" ];
+    };
+  };
+
+  env = {
+    DOCKER_BUILDKIT = "1";
+  };
+
+  services.docker.enable = true;
+}
