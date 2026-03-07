ce06bfa56bd26502a90d62b3a0cb6ec8419a5e68
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2026-03-07 21:17:36 +0800

    chore(networking): use CF Gateway DoT endpoint as fallbackDNS

--- shared/networking.nix
+++ shared/networking.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # Do a lot of systemd-resolved related chores
@@ -6,14 +11,16 @@
     "45.90.28.0#c393f6.dns.nextdns.io"
     "45.90.30.0#c393f6.dns.nextdns.io"
   ];
-  
+
   services.resolved = {
     enable = true;
     dnssec = "false"; # https://superuser.com/a/1493674
     # Commented this out since Tailscale do thee heavy work for MagicDNS
     #domains = [ "~." "fawn-cod.ts.net" ];
     # Use Cloudflare DNS resolver as fallback if things go wrong.
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    fallbackDns = [
+      "172.64.36.1#y24o2ptvff.cloudflare-gateway.com"
+    ];
     dnsovertls = "true";
   };
 
