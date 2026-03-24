59a32eae4c7264afba3ba877e50c277f406c281d
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2026-03-24 09:03:42 +0800

    chore(vscode): shell out extensions list to its own nix file and import innit for maintainability

--- shared/home-manager/vscode.nix
+++ shared/home-manager/vscode.nix
@@ -7,10 +7,7 @@
 }:
 
 let
-  inherit (pkgs.nix4vscode)
-    forVscode
-    forVscodePrerelease
-    ;
+  extensions = with ../vscode/extensions.nix; extIndex;
 in
 {
   programs.vscode = {
@@ -19,52 +16,7 @@ in
     profiles = {
       default = {
         enableExtensionUpdateCheck = true;
-        extensions =
-          forVscode [
-            # themeing
-            "GitHub.github-vscode-theme"
-            "PKief.material-icon-theme"
-            "PKief.material-product-icons"
-            "SpaceBox.monospace-idx-theme"
-
-            # tooling
-            "mkhl.direnv"
-            "github.vscode-github-actions"
-            "ms-vscode-remote.remote-ssh-edit"
-            "ms-azuretools.vscode-containers"
-            "GitHub.copilot-chat"
-            "wdhongtw.gpg-indicator"
-            "WakaTime.vscode-wakatime"
-            "ms-vscode.remote-repositories"
-            "GitHub.remotehub"
-
-            # languages: formatting, code highlighting, etc.
-            "bbenoist.Nix"
-            "arrterian.nix-env-selector"
-            "brettm12345.nixfmt-vscode"
-            "tamasfe.even-better-toml"
-            "bierner.emojisense"
-            "redhat.vscode-yaml"
-            "mads-hartmann.bash-ide-vscode"
-            "yzhang.markdown-all-in-one"
-            "bierner.markdown-checkbox"
-            "bierner.markdown-emoji"
-            "bierner.markdown-footnotes"
-            "yahyabatulu.vscode-markdown-alert"
-            "bierner.markdown-preview-github-styles"
-            "bierner.markdown-mermaid"
-            "bierner.markdown-yaml-preamble"
-            "DavidAnson.vscode-markdownlint"
-            "timonwong.shellcheck"
-            "exiasr.hadolint"
-          ]
-          ++ forVscodePrerelease [
-            # tooling
-            "eamodio.gitlens"
-            "ms-vscode.remote-server"
-            "ms-vscode-remote.remote-ssh"
-            "GitHub.vscode-pull-request-github"
-          ];
+        extensions = extensions;
       };
     };
     mutableExtensionsDir = true;
---
+++ shared/vscode/extensions.nix
@@ -0,0 +1,65 @@
+{
+  pkgs,
+  config,
+  lib,
+  self,
+  ...
+}:
+
+let
+  inherit (pkgs.nix4vscode)
+    forVscode
+    forVscodePrerelease
+    ;
+  inherit (pkgs.vscode-utils)
+    extensionFromVscodeMarketplace
+    ;
+in
+{
+  extIndex =
+    forVscode [
+      # themeing
+      "GitHub.github-vscode-theme"
+      "PKief.material-icon-theme"
+      "PKief.material-product-icons"
+      "SpaceBox.monospace-idx-theme"
+
+      # tooling
+      "mkhl.direnv"
+      "github.vscode-github-actions"
+      "ms-vscode-remote.remote-ssh-edit"
+      "ms-azuretools.vscode-containers"
+      "GitHub.copilot-chat"
+      "wdhongtw.gpg-indicator"
+      "WakaTime.vscode-wakatime"
+      "ms-vscode.remote-repositories"
+      "GitHub.remotehub"
+
+      # languages: formatting, code highlighting, etc.
+      "bbenoist.Nix"
+      "arrterian.nix-env-selector"
+      "brettm12345.nixfmt-vscode"
+      "tamasfe.even-better-toml"
+      "bierner.emojisense"
+      "redhat.vscode-yaml"
+      "mads-hartmann.bash-ide-vscode"
+      "yzhang.markdown-all-in-one"
+      "bierner.markdown-checkbox"
+      "bierner.markdown-emoji"
+      "bierner.markdown-footnotes"
+      "yahyabatulu.vscode-markdown-alert"
+      "bierner.markdown-preview-github-styles"
+      "bierner.markdown-mermaid"
+      "bierner.markdown-yaml-preamble"
+      "DavidAnson.vscode-markdownlint"
+      "timonwong.shellcheck"
+      "exiasr.hadolint"
+      "eamodio.gitlens"
+    ]
+    ++ forVscodePrerelease [
+      # tooling
+      "ms-vscode.remote-server"
+      "ms-vscode-remote.remote-ssh"
+      "GitHub.vscode-pull-request-github"
+    ];
+}
